2033940f10dfa32d93a33eb0c0f456cae1744303
BGabesz <bogdan.gabesz@gmail.com>
2022-03-11 15:10:23 +0100

    Kulonbozo sorok

--- src/kozosgitkonfgyak/KozosGitKonfGyak.java
+++ src/kozosgitkonfgyak/KozosGitKonfGyak.java
@@ -3,7 +3,9 @@ package kozosgitkonfgyak;
 public class KozosGitKonfGyak {
 
     public static void main(String[] args) {
-        String nev1, nev2, nev3;
+        String nev1;
+        String nev2;
+        String nev3;
     }
     
 }
